198320086e362499563029db5fc61042ec1c8e17
latifalbar <latif.albar13@gmail.com>
2015-12-03 06:29:09 +0700

    add tag=login

--- CyclingMaster/app/src/main/java/innoworld/cyclingmaster/Appconfig.java
+++ CyclingMaster/app/src/main/java/innoworld/cyclingmaster/Appconfig.java
@@ -5,7 +5,7 @@ package innoworld.cyclingmaster;
  */
 public class Appconfig {
     // Server login url
-    public static String URL_LOGIN = "http://192.168.1.120/tmp/phpserver/";
+    public static String URL_LOGIN = "http://192.168.173.1/phpserver?tag=login";
 
     // Server register url
     public static String URL_REGISTER = "http://192.168.1.120/tmp/phpserver/";
